124c5e4169923279952057fa260dfe9270373633
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-05-08 21:22:23 +0300

    change task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-//galleryContainer.addEventListener('click', onImgClick);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+galleryContainer.addEventListener("click", onImgClick);
 
-// rendered items
-function createGalleryItemsMarkup(items) {
-  return items
+function createGalleryItemsMarkup(elem) {
+  return elem
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -22,7 +21,34 @@ function createGalleryItemsMarkup(items) {
   </a>
 </div>`;
     })
-    .join('');
-    
+    .join("");
 }
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
 
+  if (!e.target.classList.contains("gallery__image")) return;
+
+  const currentImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+	<img src="${currentImgUrl}" width="1280" height="auto"/>
+      `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(e) {
+    if (e.code !== "Escape") return;
+    instance.close();
+  }
+}
